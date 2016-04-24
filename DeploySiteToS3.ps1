60be589218dd0b7b0e45a94aee431d786862bb22
Luke Griffith <lukemgriffith@gmail.com>
2016-04-24 23:33:41 +0100

    Jekyll config, build and deployment scripts.
    Starting a Jekyll blog, I've done a fair bit of automation for the
    deployment. VSCode tasks to build site, with PowerShell scripts being
    used to upload to S3 bucket.

---
+++ DeploySiteToS3.ps1
@@ -0,0 +1,15 @@
+﻿Import-Module "C:\Program Files (x86)\AWS Tools\PowerShell\AWSPowerShell"
+
+$VerbosePreference = 'Continue'
+
+$data = ConvertFrom-StringData (Get-Content -Path $PSScriptRoot\AWSCred.txt -Raw)
+
+$cred = New-AWSCredentials -AccessKey $data.AWSAccessKeyId -SecretKey $data.AWSSecretKey
+
+Write-Verbose "Cleaning bucket..."
+
+Get-S3Object -BucketName lgjekyllblog -KeyPrefix "\" -Credential $cred | Remove-S3Object -BucketName lgjekyllblog -Force -Credential $cred
+
+Write-Verbose "Starting Upload"
+
+Write-S3Object -BucketName lgjekyllblog -Folder "C:\Users\lukem\Documents\GitHub\JekyllBlog_Site" -Recurse -Credential $cred -KeyPrefix '\' -Verbose
